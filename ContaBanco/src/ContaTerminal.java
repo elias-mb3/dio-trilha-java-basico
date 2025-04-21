d48fde2f16538832f4ac8f377799b57a692e7bbf
elias-mb3 <monteiroelias783@gmail.com>
2025-04-21 17:53:20 -0300

    feat: :zap: Simple Create Account

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Bem vindo ao ByteBank!");
+        System.out.println("Me diga seu nome completo: ");
+        String clientName = scanner.nextLine();
+        System.out.println("agora digite o número da sua agência: XXXX");
+        int agencyNumber = scanner.nextInt();
+        System.out.println("Digite o número da sua conta: XXXXXX-X: ");
+        String accountNumber = scanner.next();
+        System.out.println("Por último, digite o saldo da sua conta: ");
+        double accountBalance = scanner.nextDouble();
+        System.out.println("Olá, " + clientName + " obrigado por criar uma conta em nosso banco, sua agência é " + agencyNumber + ", conta " + accountNumber + " e seu saldo " +accountBalance + " já está disponível para saque.");
     }
 }
